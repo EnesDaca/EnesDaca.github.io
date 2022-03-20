73dd2203b2c19c0f8abd8981cf2711daedb75255
EnesDaca <76748331+EnesDaca@users.noreply.github.com>
2022-03-20 18:42:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,148 @@
+var thumb1 =  document.getElementById("thumb1");
+var thumb1Src =  document.getElementById("thumb1").src;
+var thumb2 =  document.getElementById("thumb2");
+var thumb2Src =  document.getElementById("thumb2").src;
+var thumb3 =  document.getElementById("thumb3");
+var thumb3Src =  document.getElementById("thumb3").src;
+var thumb4 =  document.getElementById("thumb4");
+var thumb4Src =  document.getElementById("thumb4").src;
+
+thumb1.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb1Src
+})
+thumb2.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb2Src
+})
+thumb3.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb3Src
+})
+thumb4.addEventListener("click",function(){
+	document.getElementById("mainImg").src = thumb4Src
+})
+
+var thumb11 =  document.getElementById("thumb11");
+var thumb11Src =  document.getElementById("thumb11").src;
+var thumb22 =  document.getElementById("thumb22");
+var thumb22Src =  document.getElementById("thumb22").src;
+var thumb33 =  document.getElementById("thumb33");
+var thumb33Src =  document.getElementById("thumb33").src;
+var thumb44 =  document.getElementById("thumb44");
+var thumb44Src =  document.getElementById("thumb44").src;
+var thumb55 =  document.getElementById("thumb55");
+var thumb55Src =  document.getElementById("thumb55").src;
+var thumb66 =  document.getElementById("thumb66");
+var thumb66Src =  document.getElementById("thumb66").src;
+
+thumb11.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb11Src
+})
+thumb22.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb22Src
+})
+thumb33.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb33Src
+})
+thumb44.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb44Src
+})
+thumb55.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb55Src
+})
+thumb66.addEventListener("click",function(){
+	document.getElementById("mainImg2").src = thumb66Src
+})
+
+var thumb111 =  document.getElementById("thumb111");
+var thumb111Src =  document.getElementById("thumb111").src;
+var thumb222 =  document.getElementById("thumb222");
+var thumb222Src =  document.getElementById("thumb222").src;
+var thumb333 =  document.getElementById("thumb333");
+var thumb333Src =  document.getElementById("thumb333").src;
+
+thumb111.addEventListener("click",function(){
+	document.getElementById("mainImg3").src = thumb111Src
+})
+thumb222.addEventListener("click",function(){
+	document.getElementById("mainImg3").src = thumb222Src
+})
+thumb333.addEventListener("click",function(){
+	document.getElementById("mainImg3").src = thumb333Src
+})
+
+var thumbihosta1 =  document.getElementById("thumbihosta1");
+var thumbihosta1Src =  document.getElementById("thumbihosta1").src;
+var thumbihosta2 =  document.getElementById("thumbihosta2");
+var thumbihosta2Src =  document.getElementById("thumbihosta2").src;
+var thumbihosta3 =  document.getElementById("thumbihosta3");
+var thumbihosta3Src =  document.getElementById("thumbihosta3").src;
+var thumbihosta4 =  document.getElementById("thumbihosta4");
+var thumbihosta4Src =  document.getElementById("thumbihosta4").src;
+
+thumbihosta1.addEventListener("click",function(){
+	document.getElementById("mainImg4").src = thumbihosta1Src
+})
+thumbihosta2.addEventListener("click",function(){
+	document.getElementById("mainImg4").src = thumbihosta2Src
+})
+thumbihosta3.addEventListener("click",function(){
+	document.getElementById("mainImg4").src = thumbihosta3Src
+})
+thumbihosta4.addEventListener("click",function(){
+	document.getElementById("mainImg4").src = thumbihosta4Src
+})
+
+var thumbtmh1 =  document.getElementById("thumbtmh1");
+var thumbtmh1Src =  document.getElementById("thumbtmh1").src;
+var thumbtmh2 =  document.getElementById("thumbtmh2");
+var thumbtmh2Src =  document.getElementById("thumbtmh2").src;
+var thumbtmh3 =  document.getElementById("thumbtmh3");
+var thumbtmh3Src =  document.getElementById("thumbtmh3").src;
+
+thumbtmh1.addEventListener("click",function(){
+	document.getElementById("mainImg5").src = thumbtmh1Src
+})
+thumbtmh2.addEventListener("click",function(){
+	document.getElementById("mainImg5").src = thumbtmh2Src
+})
+thumbtmh3.addEventListener("click",function(){
+	document.getElementById("mainImg5").src = thumbtmh3Src
+})
+
+var thumbfaf1 =  document.getElementById("thumbfaf1");
+var thumbfaf1Src =  document.getElementById("thumbfaf1").src;
+var thumbfaf2 =  document.getElementById("thumbfaf2");
+var thumbfaf2Src =  document.getElementById("thumbfaf2").src;
+var thumbfaf3 =  document.getElementById("thumbfaf3");
+var thumbfaf3Src =  document.getElementById("thumbfaf3").src;
+
+thumbfaf1.addEventListener("click",function(){
+	document.getElementById("mainImg6").src = thumbfaf1Src
+})
+thumbfaf2.addEventListener("click",function(){
+	document.getElementById("mainImg6").src = thumbfaf2Src
+})
+thumbfaf3.addEventListener("click",function(){
+	document.getElementById("mainImg6").src = thumbfaf3Src
+})
+
+var thumbhove1 =  document.getElementById("thumbhove1");
+var thumbhove1Src =  document.getElementById("thumbhove1").src;
+var thumbhove2 =  document.getElementById("thumbhove2");
+var thumbhove2Src =  document.getElementById("thumbhove2").src;
+var thumbhove3 =  document.getElementById("thumbhove3");
+var thumbhove3Src =  document.getElementById("thumbhove3").src;
+var thumbhove4 =  document.getElementById("thumbhove4");
+var thumbhove4Src =  document.getElementById("thumbhove4").src;
+
+thumbhove1.addEventListener("click",function(){
+	document.getElementById("mainImg7").src = thumbhove1Src
+})
+thumbhove2.addEventListener("click",function(){
+	document.getElementById("mainImg7").src = thumbhove2Src
+})
+thumbhove3.addEventListener("click",function(){
+	document.getElementById("mainImg7").src = thumbhove3Src
+})
+thumbhove4.addEventListener("click",function(){
+	document.getElementById("mainImg7").src = thumbhove4Src
+})
